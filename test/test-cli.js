587cf49b38c5b420e1d8019578c38f466ff3b84d
Dawson Botsford <DawsonBotsford@gmail.com>
2016-05-21 00:43:09 -0400

    clear and set store in cli test

--- test/test-cli.js
+++ test/test-cli.js
@@ -1,11 +1,13 @@
+const path = require('path');
 import test from 'ava';
 const cliLocation = '../cli.js';
 const sh = require('shelljs');
-const fs = require('fs');
-const path = require('path');
+const Configstore = require('configstore');
+const pkg = require(path.join('..', 'package.json'));
+const conf = new Configstore(pkg.name);
+conf.set('username', 'dawsonbotsford');
 
 test('cli - valid and invalid args', t => {
-  fs.writeFileSync(path.join(__dirname, '/config.json'), `{"username": "dawsonbotsford"}`);
   t.true(sh.exec(cliLocation, {silent: true}).code === 0);
   t.true(sh.exec(cliLocation + ' opent', {silent: true, wait: false}).code === 0);
   t.true(sh.exec(cliLocation + ' opent openm', {silent: true}).code === 0);
